72c2fb446519ccc867abd916344262dab9714964
kirillbrujah <kirillbrujah@gmail.com>
2023-07-08 01:17:05 +0200

    Add isar packages to pubspec

---
+++ lib/models/attributes_models.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+// @freezed
+// class Attribute with _$Attribute {
+//   const factory Attribute({
+//     //
+// }) = _Attribute;
+//
+//
+//   factory Attribute.fromJson(Map<String, dynamic> json) =>
+//       _$AttributeFromJson(json);
+// }
--- lib/providers/attributes_providers/attributes_providers.dart
+++ lib/providers/attributes_providers/attributes_providers.dart
@@ -1,11 +1,20 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'attributes_providers.g.dart';
-
-@riverpod
-class AttributesController extends _$AttributesController {
-  @override
-  FutureOr<void> build() async {
-    return;
-  }
-}
+//
+// part 'attributes_providers.g.dart';
+//
+// @riverpod
+// class AttributesController extends _$AttributesController {
+//   @override
+//   FutureOr<void> build() async {
+//     return;
+//   }
+// }
+//
+//
+// @riverpod
+// class Attributes extends _$Attributes {
+//   @override
+//   List<String> build() {
+//     return [];
+//   }
+// }
